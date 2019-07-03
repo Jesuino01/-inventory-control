4ba5f5947ae9130f9012f900687950b6fb70dc4d
Matheus <matheus.jesuino2@gmail.com>
2019-07-03 10:58:46 -0300

    Create routes login and add header validation

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import Header from '../src/components/Header';
 import Home from './Pages/Home/Home.js';
 import Products from './Pages/Products/Products.js';
 import Register from './Pages/Register/Register.js';
+import Login from './Pages/Login/Login.js';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function isLogin() {
+  return window.location.pathname === '/login'
+}
 
 function App() {
   return (
     <Router>
       <div>
-        <Header />
+        {!isLogin() && <Header />}
 
         <Route exact path="/" component={Home} />
-        <Route path="/home" component={Home} />
+        <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <Route path="/products" component={Products} />
       </div>
